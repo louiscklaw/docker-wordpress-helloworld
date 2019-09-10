6b1c8d3c4c7f1a8777cb759228619648e5580379
louiscklaw <louiscklaw@gmail.com>
2019-09-10 18:57:02 +0800

    update,

--- docker-up.sh
+++ docker-up.sh
@@ -2,7 +2,7 @@
 
 docker network create proxy
 
-docker-compose kill && docker-compose down
+docker-compose kill && docker-compose down && docker-compose rm
 docker-compose up --remove-orphans --force-recreate -d
 
 docker-compose ps
