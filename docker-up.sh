3b449e1f985cdd4e92c72a49f86c01c49802e701
louiscklaw <louiscklaw@gmail.com>
2019-09-10 19:46:23 +0800

    update to remove volume name,

--- docker-up.sh
+++ docker-up.sh
@@ -3,6 +3,8 @@
 docker network create proxy
 
 docker-compose kill && docker-compose down && docker-compose rm
+docker volume rm db_data
+
 docker-compose up --remove-orphans --force-recreate -d
 
 docker-compose ps
